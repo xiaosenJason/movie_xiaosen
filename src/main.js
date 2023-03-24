4ac48b556485a9c30a765e7b6c2bde76b8ea53f5
xiaosen <xiaosen_jason@163.com>
2023-03-24 23:49:32 +0800

    为啥还是报错aaaaa333

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.js';
+
 
 createApp(App).use(router).mount('#app')
