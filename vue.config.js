2a3cadd1620a43960d99943397d616814464ede9
xiaosen <xiaosen_jason@163.com>
2023-03-24 23:20:37 +0800

    四次提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
