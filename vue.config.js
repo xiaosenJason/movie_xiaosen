61e81c18e2c192715091dc7f4cf9fe0a4fb880d4
xiaosen <xiaosen_jason@163.com>
2023-03-24 23:31:15 +0800

    为啥还是报错a

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': '/src',
+      'components': '/src/components'
+    },
+    extensions: ['.js', '.vue', '.json'],
+    modules: ['node_modules', './src']
   }
 })
