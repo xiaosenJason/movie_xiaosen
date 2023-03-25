eaf0ff28f4a761e0af50f5f503405175bfce4826
xiaosen <xiaosen_jason@163.com>
2023-03-25 11:12:08 +0800

    为啥还是报错aaaaa333aaa232

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,111 +1,34 @@
 <template>
-  <div class="top-nav">
-    <el-row :gutter="20">
-      <el-col :span="4">
-        <div class="logo">
-          <img src="" alt="Logo" />
-        </div>
-      </el-col>
-      <el-col :span="16">
-        <el-menu
-            :default-active="activeIndex"
-            class="el-menu-demo"
-            mode="horizontal"
-            @select="handleSelect"
-        >
-          <el-menu-item index="1">首页</el-menu-item>
-          <el-menu-item index="2">电影</el-menu-item>
-          <el-menu-item index="3">电视剧</el-menu-item>
-          <el-menu-item index="4">动漫</el-menu-item>
-          <el-menu-item index="5">综艺</el-menu-item>
-          <el-menu-item index="6">新闻</el-menu-item>
-          <el-menu-item index="7">排行榜</el-menu-item>
-        </el-menu>
-      </el-col>
-      <el-col :span="4">
-        <el-input
-            placeholder="请输入搜索关键字"
-            suffix-icon="el-icon-search"
-            clearable
-            v-model="searchText"
-            @clear="handleClear"
-            @keyup.enter.native="handleSearch"
-        ></el-input>
-      </el-col>
-    </el-row>
-  </div>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+  >
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>Workspace</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-sub-menu index="2-4">
+        <template #title>item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="3" disabled>Info</el-menu-item>
+    <el-menu-item index="4">Orders</el-menu-item>
+  </el-menu>
+
 </template>
 
-<script>
-import { reactive } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-export default {
-  setup() {
-    const state = reactive({
-      searchText: '',
-      activeIndex: '1',
-    });
-
-    const handleSelect = (index) => {
-      state.activeIndex = index;
-    };
-
-    const handleClear = () => {
-      state.searchText = '';
-    };
-
-    const handleSearch = () => {
-      console.log(`Search for ${state.searchText}`);
-    };
-
-    return {
-      state,
-      handleSelect,
-      handleClear,
-      handleSearch,
-    };
-  },
-};
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
-
-<style scoped>
-.top-nav {
-  background-color: #333;
-  padding: 10px 0;
-}
-
-.top-nav .logo {
-  font-size: 24px;
-  color: #fff;
-}
-
-.el-menu-demo {
-  background-color: #333;
-  border: none;
-  font-size: 16px;
-  margin: 0;
-}
-
-.el-menu-demo .el-menu-item {
-  color: #fff;
-}
-
-.el-menu-demo .el-menu-item:hover,
-.el-menu-demo .el-menu-item.is-active {
-  color: #ffd04b;
-}
-
-.el-input__inner {
-  background-color: #444;
-  color: #fff;
-  border: none;
-}
-
-.el-input__inner::placeholder {
-  color: #999;
-}
-
-.el-input__suffix {
-  color: #fff;
-}
-</style>
